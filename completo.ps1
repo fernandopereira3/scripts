fe7a8cceac2ea090573a54fd6ae60d895fa2a73b
Fernando <fernandopereira-3@hotmail.com>
2023-10-02 13:36:17 -0300

    first commit

---
+++ completo.ps1
@@ -0,0 +1,48 @@
+﻿cls
+echo "
+ 
+ 
+            OLA! VOU INSTALAR ALGUNS PROGRAMAS PARA VOCE ESTE PROCESSO DEMORA APROXIMADAMENTE 30 MIN.
+            VOCE PODE FAZER OUTRAS COISAS MAS ESTA MAQUINA PROVAVELMENTE VAI FICAR MAIS LENTA.
+
+                                                                                            OBRIGADO!
+ "
+sleep 10
+
+Out-Null
+
+# ATUALIZADOR DO WINDOWS
+cls
+Install-PackageProvider -Force -Confirm:$False -Name NuGet 
+Install-Module -Force -Confirm:$False PSWindowsUpdate
+
+#INSTALL WINGET
+Install-PackageProvider -Force -Confirm:$False -Name PackageManagement
+Install-PackageProvider -Force -Confirm:$False -Name Cobalt 
+Install-PackageProvider -Force -Confirm:$False -Name winget
+
+# Instalar o Cholatey
+cls 
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) > $null
+
+# Winrar
+cls
+echo "INSTALANDO O WINRAR..."
+choco install -y --force winrar
+cls
+echo "CONCLUIDO..."
+
+# Google Chrome
+cls
+echo "INSTALANDO O GOOGLE CHROME..."
+choco install -y --force googlechrome
+echo "CONCLUIDO..."
+
+# MICROSOFT OFFICE
+cls 
+echo "INSTALANDO O MICROSOFT OFFICE..."
+choco install --force -y office2019proplus --params '/Language:pt-BR'
+cls 
+echo "CONCLUIDO..."
+cls
+echo "FINALIZADO"
---
+++ update.ps1
@@ -0,0 +1,31 @@
+﻿# ATUALIZADOR DO WINDOWS
+cls
+Install-PackageProvider -Force -Confirm:$False -Name NuGet 
+Install-Module -Force -Confirm:$False PSWindowsUpdate
+
+#INSTALL WINGET
+Install-PackageProvider -Force -Confirm:$False -Name PackageManagement
+Install-PackageProvider -Force -Confirm:$False -Name Cobalt 
+Install-PackageProvider -Force -Confirm:$False -Name winget
+
+# Instalar o Cholatey
+cls 
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) > $null
+
+Install-PackageProvider -Force -Confirm:$False -Name NuGet 
+Install-Module -Force -Confirm:$False PSWindowsUpdate
+Clear-Host
+choco upgrade -y all
+Start-Sleep 10
+Clear-Host
+winrm qc -force 
+Start-Sleep 10
+Clear-Host
+Get-WindowsUpdate -Install -Confirm:$False -ForceInstall -IgnoreReboot
+Start-Sleep 10
+Clear-Host 
+winget upgrade --all
+Start-Sleep 10
+Clear-Host
+Write-Output "Atualização Finalizada..."
+Restart-Computer -Force
